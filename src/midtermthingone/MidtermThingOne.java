9bcbe8579f1407557f03b68978c9ce65f6c20833
Brandon-Kearns279 <32343611+Brandon-Kearns279@users.noreply.github.com>
2018-11-12 15:01:07 -0500

    Everything works!

---
+++ src/midtermthingone/MidtermThingOne.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermthingone;
+
+
+public class MidtermThingOne {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is a very simple program,");
+        System.out.println("all it does is print this text");
+        System.out.println("and get pushed to GitHub.");
+        
+    }
+    
+}
